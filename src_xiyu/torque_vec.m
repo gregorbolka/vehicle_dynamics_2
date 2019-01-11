f5b23d7f1aa9f50a5cbfd2467f7a78f74d7aa547
gregorbolka <46374970+gregorbolka@users.noreply.github.com>
2019-01-11 21:41:57 +0100

    Update torque_vec.m
    Automatic calculation of torque vectoring limits from desired torque

--- src_xiyu/torque_vec.m
+++ src_xiyu/torque_vec.m
@@ -1,9 +1,9 @@
 function [dTfl, dTfr, dTrl, dTrr] = torque_vec(inputs, K, angles, Ffl, Ffr, Frl, Frr, vehicle_param, idx)
 %torque_vec
-%   Generating torque vectoring signals for a double lane change
+%   Generating torque vectoring signals for a lane change
 %   Change in the steering angle is used to generate assistant torques in
 %   order to improve the transient performance during the lane change.
-%   Torque are only apply to rear wheels.
+%   Torque is only apply to rear wheels.
 %   The second part of this function is to make sure tyres are not
 %   saturated.
 
@@ -15,10 +15,12 @@ delta_old = inputs.delta(idx_old);
 dTfl = 0;
 dTfr = 0; % RWD, front wheels have no inputs
 
+T_desired = (inputs.T(3,1) + inputs.T(4,1))/2; 
+
 % a positive steering angle is turning left
 % so decrease left torque and increase right torque to get a higher yaw
 % acceleration. K is a scaling parameter to be tuned.
-dT = max(-200, min((delta_now - delta_old)*K, 200)); % -200Nm <= dT <= 200 Nm
+dT = max(-T_desired, min((delta_now - delta_old)*K, T_desired)); % -200Nm <= dT <= 200 Nm
 dTrl = -dT;
 dTrr = dT;
 
@@ -47,9 +49,9 @@ Fyrr = Frr(2);
 Fzrr = Frr(3);
 
 if abs(angles(3)) > 0.06 || sqrt(Fxrl^2+Fyrl^2) > vehicle_param.mu*Fzrl*0.75
-    dTrl = -min(200, max(180, vehicle_param.mu*Fzrl*0.8));
+    dTrl = -min(T_desired, max(T_desired*0.9, vehicle_param.mu*Fzrl*0.8));
 end
 if abs(angles(4)) > 0.06 || sqrt(Fxrr^2+Fyrr^2) > vehicle_param.mu*Fzrr*0.75
-    dTrr = -min(200, max(180, vehicle_param.mu*Fzrl*0.8));
+    dTrr = -min(T_desired, max(T_desired*0.9, vehicle_param.mu*Fzrl*0.8));
 end
 
